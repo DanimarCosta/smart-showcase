5971c2a48e5c20e09f9f58d8acf319664816d628
DanimarCosta <danimarcamposcosta@gmail.com>
2021-10-12 11:03:03 -0300

    Estrutura do código organizada

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # smart-showcase
-<h1>This showcase is nice, unlike the others, it allows interaction between the product in the store (physical) and the customer.<h1>
+<h3>This showcase is nice, unlike the others, it allows interaction between the product in the store (physical) and the customer.<h3>
 
-<h2>O que precisa ser feito ?<h2>
-<h3>Encontrar a distacia da pos x entre o centro da cabeça e as mãos<h3>
+<h4>O que precisa ser feito ?<h4>
+<h5>Encontrar a distacia da pos x entre o centro da cabeça e as mãos, para a identificação de gestos<h5>
--- application-control/main.py
+++ application-control/main.py
@@ -4,7 +4,7 @@ from cvzone.HandTrackingModule import HandDetector
 cap = cv2.VideoCapture(1)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-def hand_detector():
+def hand_distance():
     success, img = cap.read()
     #hands, img = detector.findHands(img)  # Desenha os pontos
     hands = detector.findHands(img, draw=False)  # Não desenha os pontos
@@ -12,29 +12,30 @@ def hand_detector():
     if hands:
         # Hand 1
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmarks points
-        bbox1 = hand1["bbox"]  # Bounding Box info x,y,w,h
-        centerPoint1 = hand1["center"]  # center of the hand cx,cy
-        handType1 = hand1["type"]  # Hand Type Left or Right
+        lmList1 = hand1["lmList"]  # Lista os 21 pontos
+        bbox1 = hand1["bbox"]  # Informação da caixa delimitadora x,y,w,h
+        centerPoint1 = hand1["center"]  # centro da mão cx,cy
+        handType1 = hand1["type"]  # Tipo de mão esquerda ou direita
         fingers1 = detector.fingersUp(hand1)
 
         if len(hands) == 2:
             hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmarks points
-            bbox2 = hand2["bbox"]  # Bounding Box info x,y,w,h
-            centerPoint2 = hand2["center"]  # center of the hand cx,cy
+            lmList2 = hand2["lmList"]  # Lista os 21 pontos
+            bbox2 = hand2["bbox"]  # Informação da caixa delimitadora x,y,w,h
+            centerPoint2 = hand2["center"]  # centro da mão cx,cy
             handType2 = hand2["type"]  # Mostra se a mão e a esquerda ou direita
 
             fingers2 = detector.fingersUp(hand2)
             # print(fingers1, fingers2)
-            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img) # with draw
+            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img) # Com desenho
 
-            # Distância entre as mãos
+            # Define as cordenas dos pontos e define a distancia entre as mãos
             hand1_pos_xy = lmList1[8]
             hand2_pos_xy = lmList2[8]
             pos_x = abs(hand1_pos_xy[0] - hand2_pos_xy[0])
             pos_y = abs(hand1_pos_xy[1] - hand2_pos_xy[1])
             
+            # Define a posição das mãos em relação a outra
             if pos_y >= 100:
                 id_posy = "levantadas"
             
@@ -47,10 +48,11 @@ def hand_detector():
             else:
                 id_posx = "juntas"
             
+            # Mostra o resultado da comparação lógica
             print(f"Os indicadores estao {id_posx} e {id_posy}")
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
 while True:
-    hand_detector()
+    hand_distance()
