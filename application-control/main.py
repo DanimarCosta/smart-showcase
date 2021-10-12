60ac01a47609b8ac1d0f90c9f3deda0bc0e22c1e
DanimarCosta <danimarcamposcosta@gmail.com>
2021-10-12 15:23:48 -0300

    Cria uma subrotina dentro da mão direita

--- application-control/main.py
+++ application-control/main.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(1)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 def hand_distance():
     success, img = cap.read()
@@ -42,6 +44,34 @@ def hand_distance():
     #cv2.imshow("Image", img)
     cv2.waitKey(1)
 
+def gesture_detector():
+    success, img = cap.read()
+    hands, img = detector.findHands(img)  # Desenha os pontos
+    #hands = detector.findHands(img, draw=False)  # Não desenha os pontos
+
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # Lista os 21 pontos
+        bbox1 = hand1["bbox"]  # Informação da caixa delimitadora x,y,w,h
+        centerPoint1 = hand1["center"]  # centro da mão cx,cy
+        handType1 = hand1["type"]  # Tipo de mão esquerda ou direita
+        fingers1 = detector.fingersUp(hand1)
+
+        if handType1 == "Left":
+            pass
+        
+        else:
+            # Pos_xy da mão direita
+            hand_right = lmList1[8]
+            hand_right_x = hand_right[0]
+            hand_right_y = hand_right[1]
+
+            # Procura um objeto de referencia (neste caso o rosto)
+
+    # Mostra o que esta sendo criado
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
 while True:
-    teste = hand_distance()
-    print (teste)
+    gesture_detector()
