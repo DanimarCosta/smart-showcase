9ddc65722e0e659006df0a18476697c96dea3e88
DanimarCosta <danimarcamposcosta@gmail.com>
2021-10-13 11:39:57 -0300

    Retorna o valor do movimento

--- application-control/main.py
+++ application-control/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap = cv2.VideoCapture(1)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
@@ -46,6 +47,7 @@ def hand_distance():
 
 def gesture_detector():
     hand_right_x = 0
+    face_xy = [0, 0]
     success, img = cap.read()
     hands, img = detector.findHands(img)  # Desenha os pontos
     #hands = detector.findHands(img, draw=False)  # Não desenha os pontos
@@ -79,13 +81,23 @@ def gesture_detector():
         cut_img = gray[y:y+h, x:x+w]
         cut_color = img[y:y+h, x:x+w]
     
+        # Cria um retangulo com base na pos xy e na largura e altura
         cv2.rectangle(img, (x ,y), (x + w, y + h), (0, 255, 0), 5)
-        face_xy = [x, y]
+        face_xy = [x, y] # Valores das coordenadas do rosto
         cv2.putText(img, 'Pessoa', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-    # Mostra o que esta sendo criado
+    # Exibe o movimento
+    if hand_right_x or face_xy == 0:
+        movimento_px = abs(hand_right_x - face_xy[0])
+        print(movimento_px / 10)
+   
+    else:
+        pass
+
+    # Exibe o resultado para o usuario
     cv2.imshow("Image", img)
-    
+
+    # Mostra o que esta sendo criado
     if cv2.waitKey(20) & 0xFF == ord('q'):
         return 1
 
