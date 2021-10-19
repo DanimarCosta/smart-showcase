fc3f24cedca1402bed44a71db76e69264c4370cc
DanimarCosta <danimarcamposcosta@gmail.com>
2021-10-19 20:10:00 -0300

    Montando a estrutura da aplicação

--- application-control/main.py
+++ application-control/main.py
@@ -3,9 +3,11 @@ import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 import time
 
+# Declara as Variaveis
 cap = cv2.VideoCapture(1)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 def hand_distance():
     success, img = cap.read()
@@ -101,9 +103,38 @@ def gesture_detector():
     if cv2.waitKey(20) & 0xFF == ord('q'):
         return 1024
 
+def movimentos():
+    ret, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 2)
+
+    for (x, y, w, h) in faces:
+        # Corta a imagem quando detectar e tenta reconhecer o objeto
+        cut_img = gray[y:y+h, x:x+w]
+        cut_color = img[y:y+h, x:x+w]
+    
+        # Cria um retangulo com base na pos xy e na largura e altura
+        cv2.rectangle(img, (x ,y), (x + w, y + h), (0, 255, 0), 5)
+        face_xy = [x, y] # Valores das coordenadas do rosto
+        cv2.putText(img, 'Pessoa', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+        # Detecta sorriso
+        smiles = smile_cascade.detectMultiScale(cut_img, 1.1, 20)
+        if smiles in smiles:
+            for (sx, sy, sw, sh) in smiles:
+                cv2.rectangle(cut_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+    
+    # Exibe o resultado para o usuario
+    cv2.imshow("Image", img)
+
+    # Mostra o que esta sendo criado
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        return 1024
+
 # Coloca a aplicação em um loop
 while True:
-    retorno_lógico = gesture_detector()
+    retorno_lógico = movimentos()
 
     # Controla o loop
     if retorno_lógico == 1024:
