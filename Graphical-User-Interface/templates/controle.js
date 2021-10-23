b6d033e5f9bbe45f2da64385981f8d943fcf3828
DanimarCosta <danimarcamposcosta@gmail.com>
2021-10-23 18:57:02 -0300

    Nav-Bar incorporado

--- Graphical-User-Interface/templates/controle.js
+++ Graphical-User-Interface/templates/controle.js
@@ -1,3 +1,23 @@
 function mensagens(texto){
-    alert(texto)
+    alert(texto);
+}
+
+function horario(){
+    // Obtém a data/hora atual
+    var data = new Date();
+
+    // Guarda cada pedaço em uma variável
+    var dia     = data.getDate();           // 1-31
+    var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+    var ano4    = data.getFullYear();       // 4 dígitos
+    var hora    = data.getHours();          // 0-23
+    var min     = data.getMinutes();        // 0-59
+    var seg     = data.getSeconds();        // 0-59
+
+    // Formata a data e a hora (note o mês + 1)
+    var str_data = dia + '/' + (mes+1) + '/' + ano4;
+    var str_hora = hora + ':' + min;
+    var data_hora = [str_data, str_hora]
+
+    document.write(data_hora[1])
 }
