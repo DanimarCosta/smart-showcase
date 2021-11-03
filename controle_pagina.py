f703db4dc7aabc7954eeb1de7ab44213d132db68
DanimarCosta <danimarcamposcosta@gmail.com>
2021-11-03 16:31:24 -0300

    Construindo o site

--- controle_pagina.py
+++ controle_pagina.py
@@ -1,48 +1,19 @@
-import os.path
-
-from flask import Flask, Response
-
+import os
+from flask import Flask, render_template, redirect
+from time import sleep
 
 app = Flask(__name__)
-app.config.from_object(__name__)
 
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
-def root_dir():  # pragma: no cover
-    return os.path.abspath(os.path.dirname(__file__))
+@app.route("/teste")
+def index():
+    return redirect("http://www.example.com", code=302)
+    sleep(2)
+    return redirect("http://www.google.com", code=302)
 
-
-def get_file(filename):  # pragma: no cover
-    try:
-        src = os.path.join(root_dir(), filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
-        return open(src).read()
-    except IOError as exc:
-        return str(exc)
-
-
-@app.route('/', methods=['GET'])
-def metrics():  # pragma: no cover
-    content = get_file('templates/index.html')
-    return Response(content, mimetype="text/html")
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
-
-
-app.run(port=80)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
