179f33a315956739ea021a314ac292b33343f767
DanimarCosta <danimarcamposcosta@gmail.com>
2021-11-25 19:40:30 -0300

    Criando interações na pagina e modificando o css com o javascript

--- static/controle.js
+++ static/controle.js
@@ -1,20 +1,31 @@
-const carrega_imagens = (divDestino, data) => {
-    var imagens = data.attr('imagens').split(",");
-    var html = "";
-    for(i=0;i<imagens.length;i++){
-      html = html + "<img src='" + imagens[0] + "'height=1000px'/>";
-    }
-  
-    divDestino.html(html);
-  }
-
-const mudar_cor = (cor) => {
-    document.getElementById("btn_erro").
+const mudar_cor = (componente, elemento, classe_css) => {
+    document.getElementById(elemento).
     addEventListener("click", function(){
-    document.querySelector("*").
-    setAttribute("class", cor);
+    document.querySelector(componente).
+    setAttribute("class", classe_css);
     })
 }
-const tela_azul = () => {
-  mudar_cor('azul')
+
+const status_ensino = () => {
+  mudar_cor("#cor_status_ensino", "ensino", "cor_status");
+
+  mudar_cor("#cor_status_cursos", "cursos",  "default_color");
+  mudar_cor("#cor_status_tour", "tour",  "default_color");
+  mudar_cor("#cor_status_maps", "maps", "default_color");
+  console.log("Mudança de estado no ensino");
+}
+
+const status_cursos = () => {
+  mudar_cor("#cor_status_cursos", "cursos",  "cor_status");
+  console.log("Mudança de estado no cursos");
+}
+
+const status_tour = () => {
+  mudar_cor("#cor_status_tour", "tour",  "cor_status");
+  console.log("Mudança de estado no tour");
+}
+
+const status_maps = () => {
+  mudar_cor("#cor_status_maps", "maps", "cor_status");
+  console.log("Mudança de estado no maps");
 }
