cd456dc69c81b8cd8bcd7e9feebc9428d01b7332
DanimarCosta <danimarcamposcosta@gmail.com>
2021-11-25 21:22:38 -0300

    Sistema de redirecionamentos e de ids concluido

--- static/controle.js
+++ static/controle.js
@@ -6,26 +6,29 @@ const mudar_cor = (componente, elemento, classe_css) => {
     })
 }
 
-const status_ensino = () => {
-  mudar_cor("#cor_status_ensino", "ensino", "cor_status");
 
-  mudar_cor("#cor_status_cursos", "cursos",  "default_color");
-  mudar_cor("#cor_status_tour", "tour",  "default_color");
-  mudar_cor("#cor_status_maps", "maps", "default_color");
+// Mostra o que esta selecionado
+const status_ensino = () => {
+  mudar_cor("#tag_ensino", "tag_ensino", "tag_ensino");
   console.log("Mudança de estado no ensino");
 }
 
 const status_cursos = () => {
-  mudar_cor("#cor_status_cursos", "cursos",  "cor_status");
-  console.log("Mudança de estado no cursos");
+  mudar_cor("#tag_cursos", "tag_cursos", "tag_cursos");
+  console.log("Mudança de estado no ensino");
 }
 
 const status_tour = () => {
-  mudar_cor("#cor_status_tour", "tour",  "cor_status");
-  console.log("Mudança de estado no tour");
+  mudar_cor("#tag_tour", "tag_tour", "tag_tour");
+  console.log("Mudança de estado no ensino");
 }
 
 const status_maps = () => {
-  mudar_cor("#cor_status_maps", "maps", "cor_status");
-  console.log("Mudança de estado no maps");
+  mudar_cor("#tag_maps", "tag_maps", "tag_maps");
+  console.log("Mudança de estado no ensino");
+}
+
+// Redirecionamentos de paginas
+const redirecionador = (pagina) => {
+  window.location.href = "http://127.0.0.1:5000/" + pagina;
 }
