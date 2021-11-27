126abf8f11fe54bd6f0406c2fad7f9af2b870617
DanimarCosta <danimarcamposcosta@gmail.com>
2021-11-27 15:46:37 -0300

    Integrando o back-end com o selenium

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
+from selenium import webdriver
 
 # Configurações iniciais
 cap = cv2.VideoCapture(1)
@@ -80,6 +81,54 @@ def controle(type):
     if cv2.waitKey(20) & 0xFF == ord('q'):
         return True
 
+# Pagina no Chrome
+class Bot():
+    # Inicia a classe
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
+
+    # Entra no link determinado
+    def entrar_link(self, link):
+        self.driver.get(link)
+
+    # Click nas tags de identificação    
+    def click_tag_ensino(self):
+        tag_ensino_html = self.driver.find_element_by_id("tag_ensino").click()
+        print("ok")
+    
+    def click_tag_cursos(self):
+        tag_cursos_html = self.driver.find_element_by_id("tag_cursos").click()
+
+    def click_tag_tour(self):
+        tag_tour_html = self.driver.find_element_by_id("tag_tour").click()
+
+    def click_tag_maps(self):
+        tag_maps_html = self.driver.find_element_by_id("tag_maps").click()
+    
+    # Clicks nas imagens de redirecionamento
+    def click_ensino(self):
+        img_ensino = self.driver.find_element_by_id("img_ensino").click()
+    
+    def click_cursos(self):
+        img_cursos = self.driver.find_element_by_id("img_cursos").click()
+    
+    def click_tour(self):
+        img_tour = self.driver.find_element_by_id("img_tour").click()
+    
+    def click_maps(self):
+        img_maps = self.driver.find_element_by_id("img_maps").click()
+
+    # Comando para atualizar a pagina
+    def atualizar(self):
+        self.driver.refresh()
+
+# Inicializa a pagina
+bot = Bot()
+bot.entrar_link("https://smart-showcase.netlify.app/")
+sleep(10)
+print("Vitrine Iniciada com sucesso, inicializando back-end")
+
+# Controlador da imagem
 def controlador():
     # Detecta a interação do usuario
     print("Ascene para começar")
@@ -87,6 +136,8 @@ def controlador():
         status = controle("Ativador")
         if status == True:
             print("Usuario requisitando interação")
+            bot.click_tag_ensino()
+            sleep(2)
             break
         
     # Detecta o movimento do usuario
