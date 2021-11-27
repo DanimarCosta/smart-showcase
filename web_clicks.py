bec2d8b393096be9ed1231b6537861150fb0aef2
DanimarCosta <danimarcamposcosta@gmail.com>
2021-11-27 15:29:13 -0300

    Classe de comandos para controlar a vitrine

--- web_clicks.py
+++ web_clicks.py
@@ -1,14 +1,45 @@
 from selenium import webdriver
 from time import sleep
 
-navegador = webdriver.Chrome()
-navegador.get("https://smart-showcase.netlify.app/")
+class Bot():
+    # Inicia a classe
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
 
-sleep(10)
+    # Entra no link determinado
+    def entrar_link(self, link):
+        self.driver.get(link)
 
-elemento = navegador.find_elements_by_id('tag_ensino')
+    # Click nas tags de identificação    
+    def click_tag_ensino(self):
+        tag_ensino_html = self.driver.find_element_by_id("tag_ensino").click()
+        print("ok")
+    
+    def click_tag_cursos(self):
+        tag_cursos_html = self.driver.find_element_by_id("tag_cursos").click()
 
-sleep(3)
-elemento.click()
+    def click_tag_tour(self):
+        tag_tour_html = self.driver.find_element_by_id("tag_tour").click()
 
-#navegador.refresh()
+    def click_tag_maps(self):
+        tag_maps_html = self.driver.find_element_by_id("tag_maps").click()
+    
+    # Clicks nas imagens de redirecionamento
+    def click_ensino(self):
+        img_ensino = self.driver.find_element_by_id("img_ensino").click()
+    
+    def click_cursos(self):
+        img_cursos = self.driver.find_element_by_id("img_cursos").click()
+    
+    def click_tour(self):
+        img_tour = self.driver.find_element_by_id("img_tour").click()
+    
+    def click_maps(self):
+        img_maps = self.driver.find_element_by_id("img_maps").click()
+
+    # Comando para atualizar a pagina
+    def atualizar(self):
+        self.driver.refresh()
+
+# Instruções dos bots
+bot = Bot()
